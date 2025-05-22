0fa24a402165925193e0ebff941b7e7cfd6df63d
jaremaaleksandra <jaremaaleksandra@gmail.com>
2025-05-22 12:14:17 +0200

    baza: update bazy

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Maj 21, 2025 at 03:44 PM
+-- Generation Time: Maj 22, 2025 at 10:11 AM
 -- Wersja serwera: 8.0.42-0ubuntu0.24.04.1
 -- Wersja PHP: 8.3.6
 
@@ -28,11 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cart_items` (
-  `id_cart_item` int NOT NULL,
+  `cart_item_id` int NOT NULL,
   `user_id` int NOT NULL,
   `product_id` int NOT NULL,
-  `quantity` int DEFAULT '1',
-  `order_id` int DEFAULT NULL
+  `quantity` int DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -42,11 +41,10 @@ CREATE TABLE `cart_items` (
 --
 
 CREATE TABLE `orders` (
-  `id_order` int NOT NULL,
+  `order_id` int NOT NULL,
   `order_date` date NOT NULL,
   `status` enum('dostarczono','oplacono','wyslano') DEFAULT NULL,
-  `total_amount` decimal(10,2) NOT NULL,
-  `shipping_address` text,
+  `shipping_address_id` int DEFAULT NULL,
   `delivery_date` date DEFAULT NULL,
   `user_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -58,7 +56,7 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `order_item` (
-  `id_order_item` int NOT NULL,
+  `order_item_id` int NOT NULL,
   `product_id` int DEFAULT NULL,
   `order_id` int NOT NULL,
   `quantity` int DEFAULT NULL
@@ -71,11 +69,27 @@ CREATE TABLE `order_item` (
 --
 
 CREATE TABLE `products` (
-  `id_product` int NOT NULL,
+  `product_id` int NOT NULL,
   `name` varchar(50) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `photo` text,
-  `stock_quantity` enum('jest','nie_ma') DEFAULT 'jest'
+  `stock_quantity` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `shipping_address`
+--
+
+CREATE TABLE `shipping_address` (
+  `shipping_address_id` int NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `building_number` varchar(10) NOT NULL,
+  `apartment_number` varchar(10) DEFAULT NULL,
+  `city` varchar(100) NOT NULL,
+  `postal_code` varchar(20) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -85,8 +99,8 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `users` (
-  `id_user` int NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `user_id` int NOT NULL,
+  `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `role` enum('admin','user') DEFAULT 'user',
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -99,22 +113,22 @@ CREATE TABLE `users` (
 -- Indeksy dla tabeli `cart_items`
 --
 ALTER TABLE `cart_items`
-  ADD PRIMARY KEY (`id_cart_item`),
-  ADD UNIQUE KEY `order_id` (`order_id`),
+  ADD PRIMARY KEY (`cart_item_id`),
   ADD KEY `fk_cart_items_user` (`user_id`);
 
 --
 -- Indeksy dla tabeli `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id_order`),
-  ADD KEY `fk_orders_users` (`user_id`);
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `fk_orders_users` (`user_id`),
+  ADD KEY `fk_orders_shipping_address` (`shipping_address_id`);
 
 --
 -- Indeksy dla tabeli `order_item`
 --
 ALTER TABLE `order_item`
-  ADD PRIMARY KEY (`id_order_item`),
+  ADD PRIMARY KEY (`order_item_id`),
   ADD KEY `fk_order_item_products` (`product_id`),
   ADD KEY `order_id` (`order_id`);
 
@@ -122,13 +136,19 @@ ALTER TABLE `order_item`
 -- Indeksy dla tabeli `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id_product`);
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indeksy dla tabeli `shipping_address`
+--
+ALTER TABLE `shipping_address`
+  ADD PRIMARY KEY (`shipping_address_id`);
 
 --
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`),
+  ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -139,31 +159,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart_items`
 --
 ALTER TABLE `cart_items`
-  MODIFY `id_cart_item` int NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_item_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `id_order_item` int NOT NULL AUTO_INCREMENT;
+  MODIFY `order_item_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id_product` int NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shipping_address`
+--
+ALTER TABLE `shipping_address`
+  MODIFY `shipping_address_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -173,20 +199,21 @@ ALTER TABLE `users`
 -- Constraints for table `cart_items`
 --
 ALTER TABLE `cart_items`
-  ADD CONSTRAINT `fk_cart_items_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`);
+  ADD CONSTRAINT `fk_cart_items_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `fk_orders_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`);
+  ADD CONSTRAINT `fk_orders_shipping_address` FOREIGN KEY (`shipping_address_id`) REFERENCES `shipping_address` (`shipping_address_id`),
+  ADD CONSTRAINT `fk_orders_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `order_item`
 --
 ALTER TABLE `order_item`
-  ADD CONSTRAINT `fk_order_item_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id_product`),
-  ADD CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`id_order_item`) REFERENCES `orders` (`id_order`);
+  ADD CONSTRAINT `fk_order_item_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+  ADD CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`order_item_id`) REFERENCES `orders` (`order_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
