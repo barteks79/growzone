fd135a60112e53912cb7ca9168b8aeee3c139920
Bartosz Sarnowski <sade79dev@gmail.com>
2025-05-28 20:08:00 +0200

    env: stworzono funkcje do odczytu zmiennych srodowiskowych

---
+++ php/utils.php
@@ -0,0 +1,15 @@
+<?php 
+   function get_env() {
+      $env = [];
+      $lines = file('../.env');
+
+      foreach ($lines as $line) {
+         if (!empty($line) && strpos(trim($line), '#') !== 0) {
+            list($key, $value) = explode('=', trim($line), 2);
+            $env[$key] = $value;
+         }
+      }
+
+      return $env;
+   }
+?>
