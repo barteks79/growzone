d7a4863587ac767abb69d950d4d24039c786a018
Bartosz Sarnowski <sade79dev@gmail.com>
2025-06-01 17:47:09 +0200

    frontend: dodano UI do reset_request.php

--- pages/sign-in/reset_request.php
+++ pages/sign-in/reset_request.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = $stmt->get_result()->fetch_assoc();
 
     if (!$user) {
-        echo "Jeśli konto istnieje, wysłaliśmy wiadomość.";
+        header('Location: ./reset_request.php?email=notfound');
         exit;
     }
 
@@ -105,15 +105,87 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $mail->send();
         echo "Wysłano wiadomość e-mail z linkiem do resetu.";
+        header('Location: ./reset_request.php?email=sent');
     } catch (Exception $e) {
         echo "Błąd wysyłania: {$mail->ErrorInfo}";
+        header("Location: ./reset_request.php?email=$mail->ErrorInfo");
     }
 }
 
 ?>
 
-<!-- HTML formularza -->
-<form method="POST">
-    <input type="email" name="email" required placeholder="Twój email">
-    <button type="submit">Wyślij link resetujący</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Reset Password | GrowZone</title>
+
+    <link rel="shortcut icon" href="../../public/images/icon.png" />
+    <link rel="stylesheet" href="./styles.css" />
+
+    <script src="https://unpkg.com/@tailwindcss/browser@4.1.7"></script>
+    <script src="https://unpkg.com/lucide@0.511.0"></script>
+    <script src="./script.js" defer></script>
+</head>
+
+<body class="font-[Inter]">
+    <div class="relative h-dvh">
+        <div class="primary-radial-background absolute inset-0 -z-[1]"></div>
+
+        <a href="../home/index.php" class="absolute top-6 left-6 bg-white rounded-md shadow-sm px-6 py-3">
+            <div class="flex items-center gap-3 group">
+                <img src="../../public/images/icon.png" alt="growzone icon" width="36" height="36" />
+                <span class="text-2xl font-semibold group-hover:text-emerald-600 transition">GrowZone</span>
+            </div>
+        </a>
+        <div class="h-full grid place-items-center">
+            <main class="bg-white rounded-xl shadow-xl px-8 py-8 min-w-[25rem] flex flex-col items-center">
+                <h2 class="text-3xl font-medium mb-8">Reset password</h2>
+                
+                <form method="POST" class="grid gap-3 w-full">
+                    <div class="grid gap-1 mb-3">
+                        <label for="email" class="flex gap-0.5">
+                            <span>Email Address</span>    
+                            <span class="text-red-600">*</span>
+                        </label>
+
+                        <div class="relative">
+                            <input type="email" name="email" id="email" placeholder="Enter your email" class="border border-gray-400 p-2 pl-3 rounded-md w-full" />
+                            <div class="absolute right-0.5 bottom-1 p-2">
+                                <i data-lucide="mail" class="size-[18px]"></i>
+                            </div>
+                        </div>
+
+                        <span class="text-red-600 text-sm">
+                            <?php 
+                                if (isset($_GET['email']) && $_GET['email'] !== 'sent') {
+                                    if ($_GET['email'] === 'notfound') {
+                                        echo "Ten adres email nie istnieje!"; 
+                                    } else {
+                                        echo "Coś poszło nie tak! Spróbuj ponownie później.";
+                                    }
+                                } 
+                            ?>
+                        </span>
+
+                        <span class="text-green-600 text-sm">
+                            <?php 
+                                if (isset($_GET['email']) && $_GET['email'] === 'sent') {
+                                    echo "Mail został wysłany! Sprawdź swoją skrzynkę."; 
+                                }
+                            ?>
+                        </span>
+                    </div>
+
+                    <button type="submit" class="cursor-pointer rounded-md bg-green-500 py-3 font-bold text-white shadow-xs transition hover:brightness-110 [box-shadow:0_4px_0_0_var(--color-green-700)] active:translate-y-[4px] active:shadow-none">Send</button>
+                </form>
+            </main>
+        </div>
+    </div>
+
+    <script>
+        lucide.createIcons();
+    </script>
+</body>
+</html>
