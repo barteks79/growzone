5aad972a238234fd0e842a021552b7f8a44897be
jaremaaleksandra <jaremaaleksandra@gmail.com>
2025-06-05 13:31:02 +0200

    zliczajace wartosc koszyka dla danego uzytkownika
    dupa dupa dupa = 3dupy

--- selecty.sql
+++ selecty.sql
@@ -6,3 +6,7 @@
 
 --produkt nalezacy do koszyka (po cart_id)
 -- select p.name, p.price, p.photo from products p join cart_items ci on p.product_id = ci.product_id where ci.cart_id = 1;
+
+
+--SELECT SUM(ci.quantity * p.price) AS dupa FROM carts c JOIN cart_items ci ON c.cart_id = ci.cart_id JOIN  products p ON ci.product_id = p.product_id WHERE c.user_id = 1;
+--zliczajace wartosc koszyka dla danego uzytkownika
