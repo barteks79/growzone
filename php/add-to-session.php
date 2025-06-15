982208e65f23fe67b3c3bbcd7981ca529d00bce5
Bartosz Sarnowski <sade79dev@gmail.com>
2025-06-15 11:04:09 +0200

    http: stworzono plik do zapisaywania danych do sesji

---
+++ php/add-to-session.php
@@ -0,0 +1,30 @@
+<?php 
+   session_start();
+   
+   header('Access-Control-Allow-Origin: *');
+   header('Content-Type: application/json');
+   header('Access-Control-Allow-Methods: PUT');
+   header('Access-Control-Allow-Headers: Content-Type');
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'PUT') {
+        http_response_code(405);
+        echo json_encode(['message' => 'Only PUT requests are allowed.']);
+        exit;
+    }
+
+    $body = json_decode(file_get_contents('php://input'), true);
+
+    if (!$body) {
+        http_response_code(400);
+        echo json_encode(['message' => 'No body provided.']);
+        exit;
+    }
+
+    foreach ($body as $key => $value) {
+        $_SESSION[$body] = $key;
+    }
+    
+    http_response_code(201);
+    echo json_encode(['message' => 'Session updated.']);
+    exit;
+?>
